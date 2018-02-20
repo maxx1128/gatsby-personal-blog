b32fe6f279034d89a9166228418e2b30eb23e6f3
Max Antonucci <maxx1128@gmail.com>
2018-02-20 13:18:24 -0500

    Added articles page

---
+++ src/pages/articles/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+
+class ArticlesIndex extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Articles Page!
+        </p>
+      </div>
+    )
+  }
+}
+
+export default ArticlesIndex
