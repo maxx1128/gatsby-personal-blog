82ac4d1eeb1aa22334a3ca4c6cb89c7f41db28c7
Max Antonucci <maxx1128@gmail.com>
2018-02-27 17:21:16 -0500

    Added basics to portfolio page setup

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,32 +1,38 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import get from 'lodash/get'
+import chunk from 'lodash/chunk'
+
+import s from './portfolio.module.scss'
 
 class PortfolioIndex extends React.Component {
   render() {
     const project_data = get(this, 'props.data.allProjectsYaml.edges[0].node.projects');
 
     const projects = project_data.map((project, i) => (
-        <div key={i}>
-          <li >
-            <a href={project.link} target="_blank" rel="noopener">
-              {project.name}
-            </a>
-          </li>
-          <img src={`./${project.image}`} />
-        </div>
-      )
-    );
+      <a href={project.link} target="_blank" rel="noopener" className={s.project} key={i}>
+        {<img className={s.projectImage} src="http://via.placeholder.com/400x400" />}
+        <h4 className={s.projectHeader}>
+          {project.name}
+        </h4>
+      </a>
+    ));
+
+    const grouped_projects = chunk(projects, 2).map((group) => (
+      <div className={s.projectGroup}>
+        {group}
+      </div>
+    ));
 
     return (
-      <div>
-        <p>
+      <div className={s.wrapper}>
+        <h1 className={s.title}>
           Portfolio Page!
-        </p>
+        </h1>
 
-        <ul>
-          {projects}
-        </ul>
+        <div className={s.projectContainer}>
+          {grouped_projects}
+        </div>
       </div>
     )
   }
