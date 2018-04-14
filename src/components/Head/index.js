0c07e74323d17e4976c0fdd39d249b6b98d6be55
Max Antonucci <maxx1128@gmail.com>
2018-04-14 17:34:56 -0400

    Added lang attribute to HTML

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -16,6 +16,8 @@ class Head extends React.Component {
 
     return (
       <Helmet>
+        <html lang="en" />
+
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
