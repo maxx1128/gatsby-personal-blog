d7be5d47f7aeba8688c106da98c0776bed0fc0e1
Max Antonucci <maxx1128@gmail.com>
2018-04-14 17:40:25 -0400

    Added alt text to portfolio image

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,7 +5,7 @@ const Project = ({ data, image }) => {
 
   return (
     <a href={data.link} target="_blank" rel="noopener" className={s.container}>
-      {<img className={s.image} src={image} />}
+      {<img className={s.image} src={image} alt={`Image for my '${data.name}' project`} />}
       <h4 className={s.header}>
         {data.name}
       </h4>
